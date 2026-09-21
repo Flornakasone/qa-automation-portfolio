13b7a9eab1064d6fd0ab3fd59829b05b5317bedd
Flor Naka <flornakasone@gmail.com>
2026-09-21 17:47:54 -0300

    feat(pages): add LoginPage POM and refactor login tests

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,26 +1,23 @@
 import { test, expect } from '@playwright/test'
+import { LoginPage } from '../../src/pages/LoginPage'
 
 test.describe('Login - the-internet', () => {
-    test.beforeEach(async ({ page }) => {
-        await page.goto('https://the-internet.herokuapp.com/login')
-    })
+  test('successful login with valid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    await loginPage.goto(page)
+    await loginPage.login('tomsmith', 'SuperSecretPassword!')
 
-    test('successful login with valid credentials', async ({ page }) => {
-        await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith')
-        await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!')
-        await page.getByRole('button', { name: 'Login' }).click()
-        await expect(page).toHaveTitle(/The Internet/)
-        await expect(page.getByRole('link', { name: 'Logout' })).toBeEnabled()
+    await expect(page.getByRole('heading', { name: 'Secure Area', exact: true })).toBeVisible()
+    await expect(page.getByRole('link', { name: 'Logout' })).toBeEnabled()
+    await expect(page).toHaveTitle(/The Internet/)
+  })
 
-        await expect(page.getByRole('heading', { name: 'Secure Area', exact: true })).toBeVisible()
-        await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible()
-    })
+  test('failed login shows error message', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    await loginPage.goto(page)
+    await loginPage.login('wronguser', 'wrongpassword')
 
-    test('failed login show error message', async ({ page }) => {
-        await page.getByRole('textbox', { name: 'Username' }).fill('wronguser')
-        await page.getByRole('textbox', { name: 'Password' }).fill('wrongpassword')
-        await page.getByRole('button', { name: 'Login' }).click()
-
-        await expect(page.getByText('Your username is invalid!')).toBeVisible()
-    })
+    const error = await loginPage.getErrorMessage()
+    await expect(error).toBeVisible()
+  })
 })
