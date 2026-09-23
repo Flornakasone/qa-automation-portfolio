895bef868686d93fce607ddb963e82193b00ff91
Flor Naka <flornakasone@gmail.com>
2026-09-23 13:14:54 -0300

    feat(tests): add SauceDemo login test suite with POM and fixtures

--- src/fixtures/index.ts
+++ src/fixtures/index.ts
@@ -1,8 +1,10 @@
 import { test as base } from '@playwright/test'
-import { LoginPage } from '../pages/LoginPage'
+import { LoginPage } from '../pages/the-internet/LoginPage'
+import { LoginPage as SauceDemoLoginPage } from '../pages/sauceDemo/LoginPage'
 
 type TestFixtures = {
   loginPage: LoginPage
+  sauceDemoLoginPage: SauceDemoLoginPage
 }
 
 export const test = base.extend<TestFixtures>({
@@ -10,6 +12,11 @@ export const test = base.extend<TestFixtures>({
     const loginPage = new LoginPage(page)
     await loginPage.navigate()
     await use(loginPage)
+  },
+  sauceDemoLoginPage: async ({ page }, use) => {
+    const sauceDemoLoginPage = new SauceDemoLoginPage(page)
+    await sauceDemoLoginPage.navigate()
+    await use(sauceDemoLoginPage)
   },
 })
 
